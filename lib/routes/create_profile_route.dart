6a76b91674945021c6fca7c399a84dc3905a29bd
Mika Westphal <westphal@univention.de>
2022-12-13 13:15:17 +0100

    Profile Creating Route

---
+++ lib/routes/create_profile_route.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class CreateProfileRoute extends StatelessWidget {
+  const CreateProfileRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(
+              width: double.infinity,
+              child: Card(
+                child: Text("F"),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/routes/settings_route.dart
+++ lib/routes/settings_route.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:peepofat/routes/create_profile_route.dart';
 
 class SettingsRoute extends StatelessWidget {
   const SettingsRoute({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text("Settings"),
+    return Scaffold(
+      body: ElevatedButton(
+        child: Text("F"),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const CreateProfileRoute()
+            )
+          );
+        }
+      ),
     );
   }
 }
